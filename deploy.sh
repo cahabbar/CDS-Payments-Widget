759fa64049a866082459160ae610067f0681026b
JoshMcCall <joshmccall221@gmail.com>
2018-02-27 13:10:02 -0600

    fixing dinersclub png

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 . ~/.nvm/nvm.sh; nvm use 8
+npm i -g surge ember-cli
+npm i
+ember build
 mv dist/index.html dist/200.html
-npm i -g surge
-#surge --domain demo.cds-payments.surge.sh ./dist 
-surge dist demo.cds-payments.surge.sh 
+#surge --domain demo.cds-payments.surge.sh ./dist
+surge dist demo.cds-payments.surge.sh
 
