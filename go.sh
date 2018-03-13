89a105bab43e544f5fa7c2d6e76ece212c9705e9
JoshMcCall <joshmccall221@gmail.com>
2018-03-13 14:24:17 -0500

    updates

--- go.sh
+++ go.sh
@@ -1,7 +1,11 @@
+git commit -am "updates"
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 . ~/.nvm/nvm.sh
-nvm i 8
-nvm use 8
+nvm i --lts
+nvm use --lts
+rm -rf node_modules
 npm i
-npm i -g ember-cli
-ember s
+npm i -g surge ember-cli
+ember build
+surge --domain ember.cds-payments.surge.sh ./dist
+surge --domain demo.cds-payments.surge.sh ./dist
