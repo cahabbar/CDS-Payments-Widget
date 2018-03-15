1d55c82160bd82b0e678dad495180b3b91b6dd7d
JoshMcCall <joshmccall221@gmail.com>
2018-03-15 10:25:55 -0500

    prod build

--- go.sh
+++ go.sh
@@ -1,12 +1,27 @@
-git commit -am "updates"
-git push
+# Mac / Linux
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+# blasted path is forever fail
 . ~/.nvm/nvm.sh
 nvm i --lts
 nvm use --lts
+
+# blow away and start fresh
 rm -rf node_modules
 npm i
 npm i -g surge ember-cli
-ember build
-surge --domain ember.cds-payments.surge.sh ./dist
-surge --domain demo.cds-payments.surge.sh ./dist
+
+# blow away and start fresh
+rm -rf dist docs;
+ember build --environment=production;
+mv dist docs;
+git add docs -f;
+git commit -am "prod build";
+git push;
+
+# serve things
+fuser -k 8080/tcp
+http-server ./docs & # run in background for fun
+surge --domain ember.cds-payments.surge.sh ./docs
+surge --domain demo.cds-payments.surge.sh ./docs
+ember s
