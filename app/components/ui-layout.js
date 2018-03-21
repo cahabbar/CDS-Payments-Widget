429fc9cbb29c52a7aaa17ebfffc2b7858cefb60a
JoshMcCall <joshmccall221@gmail.com>
2018-03-20 21:37:59 -0500

    demo toggle

--- app/components/ui-layout.js
+++ app/components/ui-layout.js
@@ -66,6 +66,7 @@ const stateToComputed = state => {
     cvvWarning: numberValidation.card && !validCvv(cvv).isValid,
     billMeDisabled: !!numberValidation.card,
     notAcceptedCardsString: numberValidation.card && !(acceptedCards.indexOf(numberValidation.card.type) >= 0),
+    demo:"display:none"
   };
   const configs = { ...filteredPostsBoolRes, valid };
   return {
