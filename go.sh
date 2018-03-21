41ddc586fc0074e9ee8121bcec4d902f6b7a4f26
JoshMcCall <joshmccall221@gmail.com>
2018-03-20 22:01:50 -0500

    prod build

--- go.sh
+++ go.sh
@@ -7,21 +7,21 @@ nvm i --lts
 nvm use --lts
 
 # blow away and start fresh
-#rm -rf node_modules
-#npm i
-#npm i -g surge ember-cli
+rm -rf node_modules
+npm i
+npm i -g surge ember-cli
 
 # blow away and start fresh
 rm -rf dist docs;
 ember build --environment=production;
 mv dist docs;
-#git add docs -f;
-#git commit -am "prod build";
-#git push;
+git add docs -f;
+git commit -am "prod build";
+git push;
 
 # serve things
 fuser -k 8080/tcp
 http-server ./docs & # run in background for fun
-#surge --domain ember.cds-payments.surge.sh ./docs
+surge --domain ember.cds-payments.surge.sh ./docs
 surge --domain demo.cds-payments.surge.sh ./docs
-#ember s
+ember s
