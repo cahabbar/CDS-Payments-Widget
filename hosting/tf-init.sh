5cbf2266c1b1446564204a1290b95ed3da176405
Benjamin Sherman <benjamin@trility.io>
2018-04-27 16:26:19 -0500

    AC-88 fix init

--- hosting/tf-init.sh
+++ hosting/tf-init.sh
@@ -3,9 +3,9 @@
 . env.sh
 useEnv
 
-sed '{
-    s/FEATURE_DIR/${TF_VAR_application}/
-    s/TF_VAR_account_name/${TF_VAR_account_name}/
-    s/TF_VAR_region/${TF_VAR_region}/
-}' backend.tf-template > backend.tf
+sed "{
+    s#FEATURE_DIR#${TF_VAR_application}#
+    s#TF_VAR_account_name#${TF_VAR_account_name}#
+    s#TF_VAR_region#${TF_VAR_region}#
+}" backend.tf-template > backend.tf
 terraform init
